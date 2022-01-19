13391981c1981dc2876b4be53ab4e9e28b6b876a
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2022-01-19 10:29:01 +0100

    update fixes

--- resources/views/office/show.blade.php
+++ resources/views/office/show.blade.php
@@ -73,20 +73,6 @@
                 line-height: 18px;
             }
 
-            .watermarkedd {
-              display: block;
-              width: 100%;
-              height: 100%;
-              position: absolute;
-              top: 500px;
-              left: 25%;
-              background-image: url({{ url('img/watermark.jpeg') }});
-              background-size: 500px 500px;
-              background-position: 30px 30px;
-              background-repeat: no-repeat;
-              opacity: 0.2;
-            }
-
             table {
                 border: 1px solid #000;
             }
@@ -157,7 +143,6 @@
                         </div>
                         <div class="card-body">
                             <div class="print-wrapper">
-                                <div class="watermarkedd" style="display: block;width: 100%;height: 100%;position: absolute;top: 500px;left: 25%;background-image: url({{ url('img/watermark.jpeg') }});background-size: 500px 500px;background-position: 30px 30px;background-repeat: no-repeat;opacity: 0.2;"></div>
                                 <div class="row">
                                     <div class="col-md-10- offset-1">
                                         <img src="{{asset('/img/Demandnotice.jpeg')}}" width="920px">
